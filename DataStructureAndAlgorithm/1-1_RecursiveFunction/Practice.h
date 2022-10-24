19180746eb606a7e4b4e7e5cf207c1bc5f03c5de
ChowonKim <97011721+chowon986@users.noreply.github.com>
2022-10-24 17:20:34 +0900

    김초원 : 재귀함수(2)

--- DataStructureAndAlgorithm/1-1_RecursiveFunction/Practice.h
+++ DataStructureAndAlgorithm/1-1_RecursiveFunction/Practice.h
@@ -30,6 +30,7 @@ void recursive(int count)
 //                    return;
 //                }
 //            }
+// 
 //            printf(3);
 //        }
 //        printf(2);
@@ -113,7 +114,7 @@ void PrintStar(int _StarCount)
 //}
 
 // Ex004 12.1
-void AddNumberToOne(int _StartNum)
+void PrintNumberToOne(int _StartNum)
 {
 	if (_StartNum > 20 || _StartNum <= 0)
 	{
@@ -121,7 +122,7 @@ void AddNumberToOne(int _StartNum)
 	}
 
     std::cout << _StartNum << " ";
-	AddNumberToOne(_StartNum - 1);
+    PrintNumberToOne(_StartNum - 1);
 }
 
 //ANTO(5)
@@ -158,68 +159,126 @@ void AddNumberToOne(int _StartNum)
 //}
 
 // Ex005 12.1
-void AddNumberFromOne(int _StartNum, int _EndNum)
+void PrintNumberFromOne(int _EndNum)
 {
-    if ((_EndNum - _StartNum) < 0 || _EndNum > 20)
+    if (_EndNum > 20 || _EndNum <= 0)
     {
         return;
     }
 
-    std::cout << _StartNum << " ";
-    AddNumberFromOne(_StartNum + 1, _EndNum);
+    PrintNumberFromOne(_EndNum - 1);
+    std::cout << _EndNum << " ";
 }
 
-//ANFO(1, 5)
+
+
+//
+//PNFO(5)
 //{
 //    if (false)
-//    cout(1);
-//    ANFO(2, 5)
+//    PNFO(4)
 //    {
 //        if (false)
-//        cout(2);
-//        ANFO(3, 5)
+//        PNFO(3)
 //        {
 //            if (false)
-//            cout(3);
-//            ANFO(4, 5)
+//            PNFO(2)
 //            {
 //                if (false)
-//                cout(4);
-//                ANFO(5, 5)
+//                PNFO(1)
 //                {
 //                    if (true)
 //                    {
 //                        return;
 //                    }
+//                    cout(1);
 //                }
+//                cout(2);
 //            }
+//            cout(3);
 //        }
+//        cout(4);
 //    }
+//    cout(5);
 //}
 
 // Ex007 12.1
-void FindOddNum()
+void FindOddNum(int _StartNum, int _EndNum)
 {
-    int a = 0;
-    int b = 0;
-    std::cin >> a >> b;
-
-    if (a % 2 != 0)
+    if (_StartNum >= _EndNum)
     {
-        std::cout << a << " ";
+        return;
     }
 
-    if (b % 2 != 0)
+    if (_StartNum % 2 != 0) // 홀수면
     {
-        std::cout << b << " ";
+        std::cout << _StartNum << ' ';
     }
 
-    FindOddNum();
+	FindOddNum(_StartNum + 1, _EndNum);
 }
 
+//FON(2, 7)
+//{
+//    if(false)
+//    if (false)
+//    
+//	FON(3, 7)
+//	{
+//        if (false)
+//        if (true)
+//        cout(3);
+//        
+//        FON(4, 7)
+//        {
+//            if(false)
+//            if(false)
+//
+//            FON(5, 7)
+//            {
+//                if (false)
+//                if (true)
+//                cout(5);
+//
+//                FON(6, 7)
+//                if (false)
+//                if (false)
+//
+//                FON(7, 7)
+//                if (false)
+//                return;
+//            }
+//        }
+//	}
+//}
+//
+
+
+
+//
 //FON()
 //{
+//    if (true)
+//    cout(a);
+//    if (true)
+//    cout(b);
 //    FON()
+//    {
+//        if (false)
+//        if (false)
+//        FON()
+//        {
+//            if (true)
+//            cout(a);
+//            if(false)
+//            FON()
+//            {
+//                if (false)
+//                if (true)
+//                cout(b);
+//            }
+//        }
+//    }
 //}
 
 // Ex008 12.1
@@ -262,7 +321,255 @@ void DivideByTwoUntilOne(int OriginNum)
 //}
 
 // Ex009 12.1
-void DivideByTwoCoutFromOne(int OriginNum)
+void DivideByTwoFromOne(int OriginNum)
 {
+    if (OriginNum > 0)
+    {
+        DivideByTwoFromOne((OriginNum / 2));
+        std::cout << OriginNum << ' ';
+    }
+}
+//
+//DBFO(10)
+//{
+//	if (true)
+//	{
+//		DBFO(5)
+//		if (true)
+//		{
+//			DBFO(2)
+//            if (true)
+//            {
+//                DBFO(1)
+//                {
+//                    if (true)
+//                    {
+//						DBFO(0)
+//                    }
+//                    cout(1);
+//                }
+//                cout(2);
+//            }
+//            cout(5);
+//		}
+//        cout(10);
+//	}
+//}
+
+int AddNum(int _StartNum, int _EndNum)
+{
+    if ((_StartNum < 0 || _EndNum < 0) ||
+        (_StartNum > 20 || _EndNum > 20))
+    {
+        return -1;
+    }
+
+    if (_StartNum == _EndNum)
+    {
+        return _EndNum;
+    }
+
+    return _StartNum + AddNum(_StartNum+1, _EndNum);
+}
+//
+//AN(1, 5)
+//{
+//    if (false)
+//    if (false)
+//    1 + [AddNum(2, 5) = 14]
+//    {
+//        if(false)
+//        if(false)
+//        2 + [AddNum(3, 5) = 12]
+//        {
+//            if(false)
+//            if(false)
+//            3 + [AddNum(4, 5) = 9]
+//            {
+//                if(false)
+//                if (false)
+//                {
+//                    4 + [AddNum(5,5) = 5]
+//                    if(false)
+//                    if (true)
+//                    {
+//						return 5
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    TTL return 15
+//}
+
+int AddNum(int _EndNum)
+{
+    if (_EndNum != 1)
+    {
+        return _EndNum + AddNum(_EndNum - 1);
+    }
+    else
+    {
+        return 1;
+    }
 }
 
+//AN(5)
+//{
+//    if (true)
+//    {
+//        5 + [AN(4) = 10] 
+//        {
+//            if (true)
+//            {
+//                4 + [AN(3) = 6]
+//                {
+//                    if (true)
+//                    {
+//                        3 + [AN(2) = 3]
+//                        {
+//                            if (true)
+//                            {
+//                                2 + [AN(1) = 1]
+//                                if(false)
+//                                if (true)
+//                                {
+//                                    return 1;
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
+// TTL return 15
+
+int AddEveryNum(int Num) //2571
+{
+    int a = Num / 1000;
+    int b = Num / 100 - ((Num / 1000) * 10);
+    int c = Num / 10 - ((Num / 100)* 10);
+    int d = Num - ((Num / 10) * 10);
+
+    return a + b + c + d;
+}
+
+//// 전개도
+//2571 / 1000 = 2
+//2571 / 100 = 25 - ((2571 / 1000) * 10) = 5
+//2571 / 10 = 257 - ((2571/100) * 10) = 7
+//2571 - ((2571 / 10) * 10) = 1
+
+int PrintFactorial(int Num)
+{
+    if (Num == 1)
+    {
+        return Num;
+    }
+
+    return Num * PrintFactorial(Num - 1);
+}
+
+//PF(5)
+//{
+//    if(false)
+//    return 5 * [PF(4) = 24]
+//    {
+//        if (false)
+//        {
+//            return 4 * [PF(3) = 6]
+//            {
+//                if (false)
+//                {
+//                    return 3 * [PF(2) = 2]
+//                    {
+//                        if (false)
+//                        {
+//                            return 2 * [ PF(1) = 1] 
+//                            if (true)
+//                            return 1;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
+// TTL return 120
+
+int AToThePowerB(int NumA, int NumB)
+{
+    if (1 <= (NumB))
+    {
+        return NumA * AToThePowerB(NumA, NumB - 1);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+
+//ATTPB(3, 3)
+//{
+//    if (true)
+//    {
+//        3 * [ATTPB(3, 2) = 9]
+//        {
+//            if(true)
+//            3 * [ATTPB(3, 1) = 3]
+//            {
+//                if (true)
+//                3 * [ATTPB(3, 0) = 1]
+//                return 1;
+//            }
+//        }
+//    }
+//}
+
+int MinusToZero(int _Num)
+{
+    if (0 == (_Num / 10))
+    {
+        return _Num;
+    }
+    else
+    {
+        return MinusToZero(_Num / 10);
+    }
+}
+
+int Count(int _Num, int _Count)
+{
+    if (_Num == 0)
+    {
+        return _Count;
+    }
+
+    return Count(_Num - MinusToZero(_Num), ++_Count);
+}
+//
+//// 전개도
+//CUZ(13, 0)
+//{
+//    CUZ(13 - [MTZ(13) = 1], 1)
+//    {
+//        CUZ(12 - [MTZ(12) = 1], 2)
+//        { 
+//            CUZ(11 - [MTZ(11) = 1], 3)
+//            {
+//                CUZ(10 - [MTZ(10) = 1], 4)
+//                {
+//                    CUZ(9 - [MTZ(9) = 9], 5)
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//// TTL return 5
--- DataStructureAndAlgorithm/1-1_RecursiveFunction/main.cpp
+++ DataStructureAndAlgorithm/1-1_RecursiveFunction/main.cpp
@@ -9,11 +9,17 @@ int main(void)
     //recursive(1);
     //SayHello(3);
     //PrintStar(3);
-    //AddNumberToOne(5);
-    //AddNumberFromOne(1,5);
-    //FindOddNum();
-    int OriginNum;
-    std::cin >> OriginNum;
-    DivideByTwoCoutFromOne(OriginNum);
+    //PrintNumberToOne(5);
+    //PrintNumberFromOne(5);
+    //FindOddNum(1, 7);
+    //DivideByTwoUntilOne(10);
+    //DivideByTwoFromOne(10);
+    //std::cout << AddNum(1, 5);
+   // std::cout << AddNum(5);
+   // std::cout << AddEveryNum(2571);
+    //std::cout << PrintFactorial(5);
+   // std::cout << AToThePowerB(3, 3);
+    std::cout << Count(13,0);
     return 0;
+
 }
